9930dd0e61587b02627b21e839c5e9e0f615166e
Divya-021 <90896647+Divya-021@users.noreply.github.com>
2024-07-05 20:25:29 +0530

    Add files via upload

---
+++ javaTest/PubC.java
@@ -0,0 +1,13 @@
+package com.kiet.javaTest;
+import com.kiet.javaDemo.C1;
+public class PubC extends C1{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//C1 ob=new C1();
+		PubC ob=new PubC();
+		ob.c=20;
+		ob.inc();
+	}
+
+}
